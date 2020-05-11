dab5868d3dafa6ac1669e73f53d123e7763e576b
Seidai Inoue <shengdajingshang@gmail.com>
2020-05-11 16:24:16 +0900

    resourcesを使うCRUDアプリ

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get "/users", to:"users#index"
-  get "/users/new", to:"users#new"
-  post "/users",  to:"users#create"
-  delete "/users/:id", to:"users#destroy"
-  get "/users/:id/edit", to:"users#edit"
-  patch "/users/:id", to:"users#update"
+  resources :users
 end
